e57b0237f00ef37f0f1149756e3c4d09c46c8206
Askar Serikov <askar.k.serikov@gmail.com>
2021-09-16 14:27:10 +0200

    testing self-mutation

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -10,7 +10,7 @@ export class CdkPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('askarserikov/cdk-pipeline', 'feature1'),
+        input: CodePipelineSource.gitHub('askarserikov/cdk-pipeline', 'master'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
